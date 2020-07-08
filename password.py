1430535a5108ae1930b81ef800ec874942615322
Mzfr <muzaffer.ayd38@gmail.com>
2020-07-09 01:19:12 +0300

    Password klasorunu komit ettim

---
+++ password.py
@@ -0,0 +1,10 @@
+name = input("Please input your name :")
+var1 = "Joseph"
+var2 = "w@12"
+message = f"My name is {var1} and my password is {var2}"
+
+
+if name != var1 :
+    print("Hello ",name," See you later")
+else:
+    print("Hello ",name,"your password ", var2 )
